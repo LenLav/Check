8712d337642d4c1a2f283f974bc780d28a934c36
anononana <anna.vlasova.01@gmail.com>
2022-01-14 20:04:49 +0300

    с библиотекой

--- src/Api/index.js
+++ src/Api/index.js
@@ -1,24 +1,43 @@
 import axios from 'axios'
 
-// const url1 = "https://api.stage.vdpaybox.ru/api/info/receipt/lena"
-// const url2 = "https://api.stage.vdpaybox.ru/api/info/receipt/202f:57:00"
+const url1 = "https://api.stage.vdpaybox.ru/api/info/receipt/lena"
+const url2 = "https://api.stage.vdpaybox.ru/api/info/receipt/202f:57:00"
 
-// const urlAnna = "https://api.stage.vdpaybox.ru/api/receipt/deviceInfo/RiPo_ele11dc"
-
-const id_check = "RiPo_ele14dc"
+const id_check = "RiPo_ele11dc"
 const url = "https://api.stage.vdpaybox.ru/api/receipt/deviceInfo/" + id_check
+
+const url3 = "https://api.stage.vdpaybox.ru/api/receipt/deviceInfo/large21"
+
+
+
+// export async function getReceiptByUrl(){
+//     const url = window.location.href; //Сохраняешь ссылку в переменную
+//     const newUrl = url.replace(
+//       'http://localhost:3000/#', //Строка или regexp того что надо заменить
+//       "https://api.stage.vdpaybox.ru" // На что надо заменить
+//     );
+
+
+
+
+
+
+// window.location.replace( 'https://api.stage.vdpaybox.ru/api/receipt/deviceInfo/_RiPo_ele12dc');
+
+const URL = window.location.href
+console.log(URL)
+
 
 export const fetchCheck = async() => {
     try{
-        // const data = await axios.get(url1)
-        // const response = await axios.get(url)
-        const response = await axios.get(url)
-        console.log(response.data)
-        return response.data
         
+
+        const response = await axios.get(url3)
+        console.log(response.data.result)
+        return response.data.result
     }
     catch(error){
-        console.log(error.response.data)
+        // console.log(error.response.data)
         return "error"
     }
 }
--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.scss';
-// import logo from './Logo_vti_1.png';
-import logo from './img/Logo_vti_1.png';
+// import logo from './img/Logo_vti_1.png';
 
 import React, { useEffect, useState } from 'react';
 import { fetchCheck } from './Api/index'
@@ -9,22 +8,26 @@ function App() {
   const [Data, setData] = useState([]);
   const [CheckOrg, setCheckOrg] = useState(1);
   const [CheckTovar, setCheckTovar] = useState([]);
-  const [count, setCount] = useState(2);
-  const [rez, setrez] = useState([]);
+  const [count, setCount] = useState(0);
   const [Sum, setSum] = useState([]);
   const [Ofd, setOfd] = useState([]);
-  const [TaskJson, setTaskJson] = useState([]);
   const [СashboxModel, setСashboxModel] = useState([]);
+
+  const [Rez, setRez] = useState(null);
+
+  const [PaymentMethod, setPaymentMethod] = useState([]);//dictionaries PaymentMethod
+  const [PaymentObject, setPaymentObject] = useState([]);//dictionaries PaymentObject
+  const [NDS, setNDS] = useState([]);//dictionaries NDS
 
   const [Fn, setFn] = useState([]);
   const [Sno, setSno] = useState([]);
-  const [TradePlace, setTradePlace] = useState(1);
+  const [TradePlace, setTradePlace] = useState();
 
   const [clientInfo, setclientInfo] = useState();
-  const [Electronically, setElectronically] = useState();
+  const [TypePay, setTypePay] = useState();
   const [Type_check, setType_check] = useState();
 
-  const EmailHref = "https://e.mail.ru/compose/?mailto=mailto%3A" + CheckOrg.email;
+  // const EmailHref = "https://e.mail.ru/compose/?mailto=mailto%3A" + CheckOrg.email;
 
 
   
@@ -40,154 +43,239 @@ function App() {
     console.log("pag")
   }
 
-  
+  // const [response, setResponse] = useState(null);
 
    useEffect(() => {
       const fetch = async () => {
       const response = await fetchCheck();
-      console.log(response)
+
+      // setResponse(await fetchCheck())
+     
 
       if(response === "error"){
-
-        console.log("pagepagepag")
-        // return <UserGreeting />;
-        // return(
-        //   <div>page page pagepagepag epagepagepagepagepa gepagepagepagepagepagep agepagepagepagepage pagepagepagepagepage</div>
-        // );
-
-
         document.getElementById('success').hidden = true
         document.getElementById('error').hidden = false
-        // return <UserGreeting />;
         return
       }
-
-      if(response.success === true){
+      else{
         document.getElementById('success').hidden = false
         document.getElementById('error').hidden = true
       }
 
+      
 
-      setData(response.result);
-      setCheckOrg(response.result.organization);
-      setCheckTovar(response.result.taskJson.parameters.items);
-      setrez(response.result);
-      setSum(response.result.taskJson.parameters.payments[0].sum);
-      setclientInfo(response.result.taskJson.parameters.clientInfo.emailOrPhone)
+      console.log(response.data)
+      console.log(response.data.id)
+      setRez(response.data)
+      console.log(Rez)
+      // console.log(Rez.id)
 
-      setFn(response.result.fn)
-      setSno(response.result.sno)
-      // setTradePlace()
+      setData(response.data);
+      setCheckOrg(response.data.organization);
+      setCheckTovar(response.data.taskJson.parameters.items);
+      setSum(response.data.taskJson.parameters.payments[0].sum);
+      setclientInfo(response.data.taskJson.parameters.clientInfo.emailOrPhone)
+
+      setFn(response.data.cashbox.fn)
+      setSno(response.data.sno)
 
       // console.log(response.result.taskJson.parameters.items)
+      
+      setOfd(response.data.ofd);
+      setСashboxModel(response.data.cashbox.cashboxModel);
 
-      setCount(10)
-      setOfd(response.result.ofd);
-      setTaskJson(response.result.taskJson.parameters.operator);
-      setСashboxModel(response.result.cashbox.cashboxModel);
-      // console.log(response.result.taskJson)
 
-      setType_check(response.result.receiptType.name)
+      setType_check(response.data.receiptType.name)
 
-      if (response.result.taskJson.parameters.electronically === true) {
-        setElectronically('Безналичными')
+
+      if (response.data.taskJson.parameters.payments[0].type === 'cash') {
+        setTypePay('Наличными')
       }
-      else { setElectronically('Наличными') }
+      if (response.data.taskJson.parameters.payments[0].type === 'electronically') {
+        setTypePay('Безналичными')
+      }
 
-      if (response.result.taskJson.tradePlace === null) {
-        document.getElementById('TradePlace').hidden = false
+      // else { setElectronically('Безналичными') }
+
+      if (response.data.tradePlace === null) {
+        document.getElementById('TradePlace1').hidden = true
+        document.getElementById('TradePlace2').hidden = true
+        // setTradePlace("–")
       }
       else { 
-        document.getElementById('TradePlace').hidden = true
-        setTradePlace(response.result.taskJson.tradePlace)
+        document.getElementById('TradePlace1').hidden = false
+        document.getElementById('TradePlace2').hidden = false
+        setTradePlace(response.data.tradePlace)
+        console.log(TradePlace)
       }
+
+      console.log(response.data.taskJson.parameters.items.length)
+
+      for (let i = 0; i < response.data.taskJson.parameters.items.length; i++) {
+        const element = response.data.taskJson.parameters.items[i];
+        console.log(element.paymentMethod)
+        console.log(element.paymentObject)
+
+        for (let pM = 0; pM < response.dictionaries.paymentMethod.length; pM++) {
+          if(element.paymentMethod === response.dictionaries.paymentMethod[pM].eng){
+            
+          }
+          
+        }
+        
+      }
+
+
+      setPaymentMethod(response.dictionaries.paymentMethod)
+      setPaymentObject(response.dictionaries.paymentObject)
+      setNDS(response.dictionaries.tax)
+
+      console.log(response.dictionaries.paymentObject)
+
+     
+
+  
+
     }
     fetch();
   }, [])
+
+
+
 
   
 
 
   return (
+    <body>
     <div className="App">  
 
     <div id='success' hidden>  
 
-      <div className='check' id='check'>  
+      <div className='check zig-zag' id='check'>  
 
         <h2 className='check__h2'>{Type_check}</h2>
 
-        <hr className="check__hr"></hr>
+        <hr className="hr-two-gradient"></hr>
 
         <div className='check__div'>
-            <div className='check__div-1'>КАССОВЫЙ ЧЕК № {Data.id}</div>
+            <div className='check__div-1'>Дата</div>
             <div className='check__div-2'>{new Date(Data.fnDateTime).toLocaleString().substring(0, new Date(Data.fnDateTime).toLocaleString().length - 3) }</div>
-            
+           
             <div className='check__div-wrap'></div>{/* перенос */}
 
             <div className='check__div-1'>Смена</div>
             <div className='check__div-2'>{Data.shift}</div>
+
             
-            <div className='check__div-wrap'></div>{/* перенос */}
 
-            <div className='check__div-1'>Модель кассы</div>
-            <div className='check__div-2'>{СashboxModel.model}</div>
 
             <div className='check__div-wrap'></div>{/* перенос */}
 
-            <div className='check__div-1'>Фискальный признак</div>
+            <div className='check__div-1'>ФД</div>
+            <div className='check__div-2'>{Data.fdn}</div>
+
+            <div className='check__div-wrap'></div>{/* перенос */}
+
+            <div className='check__div-1'>ФП</div>
             <div className='check__div-2'>{Data.fpd}</div>
-            
+
             <div className='check__div-wrap'></div>{/* перенос */}
 
-            <div className='check__div-1'>Фискальный накопитель</div>
+            <div className='check__div-1'>ФН</div>
             <div className='check__div-2'>{Fn.number}</div>
 
             <div className='check__div-wrap'></div>{/* перенос */}
 
-            <div className='check__div-1'>Оператор фискальных данных</div>
-            <div className='check__div-2'>{Ofd.name}</div>
-            
-            <div className='check__div-wrap'></div>{/* перенос */}
-
-            <div className='check__div-1'>ИНН ОФД</div>
-            <div className='check__div-2'>{Ofd.inn}</div>
-
-            <div className='check__div-wrap'></div>{/* перенос */}
-
-            <div className='check__div-1'>Система налогообложения </div>
+            <div className='check__div-1'>СНО</div>
             <div className='check__div-2'>{Sno.name}</div>
             
             <div className='check__div-wrap'></div>{/* перенос */}
 
-            <div className='check__div-1' id='TradePlace'>Торговая площадка</div>
-            <div className='check__div-2'>{TradePlace}</div>
+            <div className='check__div-1'>ОФД</div>
+            <div className='check__div-2'>{Ofd.name}</div>         
+            
+            <div className='check__div-wrap'></div>{/* перенос */}
+
+              <div className='check__div-1' id='TradePlace1' hidden>Торговая площадка</div>
+              <div className='check__div-2' id='TradePlace2' hidden>{TradePlace}</div>
         </div>
 
-        <hr className="check__hr"></hr>
+        <hr className="hr-two-gradient"></hr>
 
           <div className='check__div'>
             <div className='check__div-tovar-1 info'>№</div>
-            <div className='check__div-tovar-2 info'>Наименование</div>
+            <div className='check__div-tovar-2 info' >Наименование</div>
             <div className='check__div-tovar-3 info'>Сумма</div>
           </div>
 
+          <ol  style={{margin: 0, paddingLeft: 0}}>
+
           {CheckTovar.map((data) =>
-            <div className='check__div'>
-            <div className='check__div-tovar-1'>1.</div>
-            <div className='check__div-tovar-2'>{data.name}</div>
-            <div className='check__div-tovar-3'>{data.price}.00₽ × {data.quantity} <br/>= <b>{data.amount}.00</b>₽</div>
+            <div>
+
+            <div className='check__div' style={{flexWrap: 'nowrap'}}>
+            <div className='check__div-tovar-1'><li style={{listStylePosition: 'inside'}}></li></div>
+            <div className='check__div-tovar-2'>
+              <div className='div_word-wrap'>{data.name}
+              </div>
+              </div>
+            <div className='check__div-tovar-3'>
+              {data.price}.00₽ × {data.quantity} = <span className='color-darckgreen'>{data.amount}.00</span>₽
+              </div>
             </div>
+
+            <div className='check__div' style={{flexWrap: 'nowrap'}}>
+            <div className='check__div-tovar-1'></div>
+
+
+            
+
+            {PaymentObject.map((object) =>
+            data.paymentObject == object.eng ?
+            <div className='check__div-tovar-2'>{object.name}</div>  :   <p></p>
+             )} 
+
+            {PaymentMethod.map((method) =>
+            data.paymentMethod == method.eng ?
+            <div className='check__div-tovar-3'>{method.name}</div>  :   <p></p>
+             )}
+
+
+            </div>
+
+            <div className='check__div' style={{flexWrap: 'nowrap'}}>
+            <div className='check__div-tovar-1'></div>
+            <div className='check__div-tovar-2'></div>
+
+            {NDS.map((nds) =>
+            data.tax.type == nds.eng ?
+            <div className='check__div-tovar-3'>НДС: {nds.name}</div>  :   <p></p>
+             )}
+
+
+            {/* {data.tax.type == 'none' ?
+            <div className='check__div-tovar-3' >НДС: без НДС</div> : 
+            <div className='check__div-tovar-3'>НДС: c НДС</div>} */}
+            </div>
+
+            <hr className='hr-horizontal-gradient'></hr>
+
+            </div>
+            
           )}
 
-          <div className='check__div'>
+          </ol>
+
+
+          <div className='check__div' style={{paddingTop: 20}}>
             <div className='check__div-1 info'>ИТОГО</div>
             <div className='check__div-2 info'>{Sum}.00₽</div>
             
             <div className='check__div-wrap'></div>{/* перенос */}
 
-            <div className='check__div-1'>Безналичными</div>
-            <div className='check__div-2'>{Sum}.00₽</div>
+            <div className='check__div-1'>{TypePay}</div>
+            <div className='check__div-2'>{}.00₽</div>
             
             <div className='check__div-wrap'></div>{/* перенос */}
 
@@ -196,11 +284,11 @@ function App() {
             
             <div className='check__div-wrap'></div>{/* перенос */}
 
-            <div className='check__div-1'>Эл. адрес или тел. получателя</div>
+            <div className='check__div-1'>Email/телефон получателя</div>
             <div className='check__div-2'>{clientInfo}</div>
           </div>
 
-          <hr className="check__hr"></hr>
+          <hr className="hr-two-gradient"></hr>
 
           <div className='check__div'>
             <div className='check__div-1 info'>Организация</div>
@@ -218,10 +306,6 @@ function App() {
 
             <div className='check__div-wrap'></div>{/* перенос */}
 
-            {/* <div className='check__div-1'>
-              <img src={logo} alt="logo" className='check__img'></img>
-            </div>
-            <div className='check__div-2'style={{color: "#2a1b75", fontSize: 20}}><b>(4712) 400-900</b></div> */}
           </div>
 
           {/* <div className='check__div'>
@@ -234,33 +318,32 @@ function App() {
 
       </div>
      
-      <div className="div_email">
-
-      <p className="p_email">Вы можете отправить чек на свой email:</p>
-
-      <input className="input_email" placeholder="Адрес вашей электронной почты"></input>
-      <button className="app__button" onClick={click}>Отправить</button>
-
-      </div>
-
       </div> 
-
+     
       
-      
 
-      <div id='error' hidden>
-        <div className='error__div'>
-          <h1 className='error__h1'><hr className='hr-dotted'></hr>Чек по указанной ссылке не найден.<hr className='hr-dotted'></hr></h1>
-          {/* <hr className='hr-dotted'></hr> */}
-          <p className='error__p'>Пожалуйста, подождите, Ваш чек скоро появится.</p>
-          <p className='error__p'>Или проверьте правильность введенной ссылки.</p>
+      <div className='check zig-zag' id='error' hidden>
+        <div className='error__div' style={{ marginTop: 0}}>
+          <h1 style={{fontWeight: 300, marginTop: 0}}><hr className='hr-dotted'></hr>Чек скоро появится<hr className='hr-dotted'></hr></h1>
+          {/* <p>Пожалуйста, подождите, Ваш чек скоро появится.</p> */}
+          <p>если адрес указан верно</p>
         </div>
-        <button onClick={click2} className="app__button__error">Повторить попытку</button>
+        <button onClick={click2} className="app__button-error">Повторить попытку</button>
       </div>
+
+
+      {/* {1 == 1 ?  
+          <p >qwertyuio</p> : 'unable to map' }  */}
+          
+        {/* {console.log(CheckTovar)} */}
+
+        {/* {console.log(this.handleCheck('item2'))} */}
+        
 
       
 
     </div>
+    </body>
   );
 }
 
